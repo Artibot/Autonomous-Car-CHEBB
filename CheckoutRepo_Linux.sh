aef55ec467ca392d70077f46c5bcb81427870e89
EndHa <liubozhao@gmail.com>
2019-04-04 15:34:39 +0200

    tune controoler #86

--- CheckoutRepo_Linux.sh
+++ CheckoutRepo_Linux.sh
@@ -1,6 +1,8 @@
 git pull
 cd ..
-IF not exist Dependencies (git clone https://github.com/Artibot/Dependencies.git)
+if [ ! -d  ]; then
+git clone https://github.com/Artibot/Dependencies.git
+fi
 cd Dependencies
 git fetch
 git checkout 7834e44faa6f1a32b8ca95124d5c91d422797eb4
--- Main.py
+++ Main.py
@@ -1,33 +1,40 @@
 import sys
-	sys.path.append('../Dependencies')
+sys.path.append('../Dependencies')
 import pygame
 from steering import Steering
+from time import sleep
 
 pygame.init()
 pygame.joystick.init()
 
-Joystick = pygame.joystick.Joystick(0)
-Joystick.init()
+
 
 SteeringWheel = Steering(388)
 SteeringWheel.Run()
 
 while 1:
-	autoMode = Joystick.get_button(6)
-	if autoMode:
-		#auto predict
-		
-	else:
-		#manual override
-		axis = Joystick.get_axis(0)
-		start = Joystick.get_button(7)
-		if start:
-			SteeringWheel.set_angle(axis)
-		sleep(0.0001)
-		stop = Joystick.get_button(0)
-		if stop:
+	pygame.event.get()		
+
+	if pygame.joystick.get_count() == 1:
+		Joystick = pygame.joystick.Joystick(0)
+		Joystick.init()
+		autoMode = Joystick.get_button(6)
+		if autoMode:
+			#auto predict
 			break
-			
+		else:
+			#manual override
+			axis = Joystick.get_axis(0)
+			start = Joystick.get_button(7)
+			if start:
+				SteeringWheel.set_angle(axis)
+		
+			stop = Joystick.get_button(0)
+			if stop:
+				break
+		sleep(0.001)
+		del Joystick
+		
 SteeringWheel.Stop()
 del SteeringWheel
-del Joystick
+
